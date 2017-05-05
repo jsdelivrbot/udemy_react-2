ac75572382d8db09772286a9d5c6f3328f0a0b1a
Chris <synthetic_samurai@hotmail.com>
2017-05-04 23:45:38 -0400

    updated index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'React-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBFUSpk9wq5M799B7U8ZgR_L1oEfTRogXw';
 
-//create a new component. This component should produce some html
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+});
+
+class App extends Component {
+	render(){
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
-//take this component's generated html and put it on the page ( in the dom )
 ReactDOM.render(<App />, document.querySelector('.container'));
